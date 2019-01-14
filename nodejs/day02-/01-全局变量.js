1970c37d3bd030887f144cd6e1662380b4ea8242
newluxiaobo <915276584@qq.com>
2019-01-14 14:09:58 +0800

    message

---
+++ nodejs/day02-/01-全局变量.js
@@ -0,0 +1,17 @@
+var happyDay = "1月14日"
+console.log(happyDay);
+
+//全局变量 全局使用的变量
+
+console.log(__filename);
+console.log(__dirname);
+
+//
+setTimeout(() => {
+   console.log("3s login...");
+    
+}, 3000);
+
+console.log("end...");
+
+
---
+++ nodejs/day02-/03-核心模块.js
@@ -0,0 +1,45 @@
+/**
+ * 模块 它是Nodejs中很重要的一个概念
+ * 为了能让nodejs文件可以相互调用,nodejs提供了一个简单模块系统
+ * 模块是nodejs应用程序的基本组成部分,文件和模块是一一对应的
+ * 也就是说一个js文件就是一个模块
+ * require
+ */
+// nodejs 自身自带的模块(系统级别,内置)
+// fs = file system 文件系统
+//一般来说,对服务器的访问其实就是访问文件
+
+
+// 如果相对文件进行操作,必须先引入文件模块
+const fs = require("fs")
+
+//open(path,flags,mode,callback)
+/**
+ * path: 指定文件路径("相对、绝对")
+ * flags:指定打开文件的方式 r 只读  w 只写 r+ 读方式,如果文件不存在则会抛出异常
+ * w+ 如果文件不存在 则创建文件
+ * rs 以同步的方式进行读取
+ * rs+ 以同步的方式进行读取和写入文件
+ * a 以追加的方式打开一个文件,如果文件不存在就创建
+ * a+ 同a
+ * callback: 用来处理打开过程中的状态
+ *      err: 错误信息
+ *       fd: 表示打开后,操作文件的对象
+ */
+fs.open("./text.txt","r+",(err,fd)=>{
+    if(err){
+        console.log("err:"+err);
+        throw err;
+    }
+    console.log("打开成功");
+    
+})
+
+//先读
+fs.readFile("./text.txt",(err,fd)=>{
+    if(err){
+        throw err
+    }
+    console.log(fd.toString());
+})
+
